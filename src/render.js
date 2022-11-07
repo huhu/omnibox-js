ad68718bad041884dcba58dbf7715929a4086a49
Folyd <lyshuhow@gmail.com>
2022-11-07 10:37:55 +0800

    Fix suggest function binding

--- src/render.js
+++ src/render.js
@@ -1,21 +1,34 @@
 class Render {
-    constructor({inputElement}) {
+    constructor({ inputElement }) {
         this.onInputChanged = new OnInputChangedListener();
         this.onInputEntered = new OnInputEnteredListener();
 
         this.inputBox = document.querySelector(inputElement);
+        let suggestFn = this.suggest.bind(this);
         this.inputBox.oninput = async (event) => {
             let input = event.target.value;
-            for (const listener of this.onInputChanged.listeners) {
-                await listener(input, this.suggest);
+            if (input) {
+                for (const listener of this.onInputChanged.listeners) {
+                    await listener(input, suggestFn);
+                }
+            } else {
+                let dropdown = document.querySelector('.omnibox-js-dropdown');
+                if (dropdown) {
+                    dropdown.remove();
+                }
             }
         };
     }
 
     suggest(suggestions) {
+        let items = [];
         for (let { content, description } of suggestions) {
             console.log(content, description);
+            items.push(`<li class="">${description}</li>`);
         }
+        this.inputBox.insertAdjacentHTML('afterend', `
+            <ul class="omnibox-js-dropdown">${items.join('')}</ul>
+        `);
     }
 }
 
