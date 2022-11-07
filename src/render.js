d44b8b918ca3c5197d23fe78570c02f397acca7f
Folyd <lyshuhow@gmail.com>
2022-11-07 19:23:18 +0800

    Parse omnibox description

--- src/render.js
+++ src/render.js
@@ -24,7 +24,7 @@ class Render {
         let items = [];
         for (let { content, description } of suggestions) {
             console.log(content, description);
-            items.push(`<li class="">${description}</li>`);
+            items.push(`<li class="">${parseOmniboxDescription(description)}</li>`);
         }
         this.inputBox.insertAdjacentHTML('afterend', `
             <ul class="omnibox-js-dropdown">${items.join('')}</ul>
@@ -54,4 +54,63 @@ class OnInputEnteredListener {
             this.listeners.push(listener);
         }
     }
+}
+
+// Remove invalid characters from text.
+function sanitizeString(text, shouldTrim) {
+    // NOTE: This logic mirrors |AutocompleteMatch::SanitizeString()|.
+    // 0x2028 = line separator; 0x2029 = paragraph separator.
+    let removeChars = /(\r|\n|\t|\u2028|\u2029)/gm;
+    if (shouldTrim)
+        text = text.trimLeft();
+    return text.replace(removeChars, '');
+}
+
+function parseOmniboxDescription(input) {
+    let domParser = new DOMParser();
+
+    // The XML parser requires a single top-level element, but we want to
+    // support things like 'hello, <match>world</match>!'. So we wrap the
+    // provided text in generated root level element.
+    let root = domParser.parseFromString(
+        '<fragment>' + input + '</fragment>', 'text/xml');
+
+    // DOMParser has a terrible error reporting facility. Errors come out nested
+    // inside the returned document.
+    let error = root.querySelector('parsererror div');
+    if (error) {
+        throw new Error(error.textContent);
+    }
+
+    // Otherwise, it's valid, so build up the description result.
+    let description = '';
+
+    // Recursively walk the tree.
+    function walk(node) {
+        for (let i = 0, child; child = node.childNodes[i]; i++) {
+            // Append text nodes to our description.
+            if (child.nodeType === Node.TEXT_NODE) {
+                let shouldTrim = description.length === 0;
+                description += sanitizeString(child.nodeValue, shouldTrim);
+                continue;
+            }
+
+            // Process and descend into a subset of recognized tags.
+            if (child.nodeType === Node.ELEMENT_NODE &&
+                (child.nodeName === 'dim' || child.nodeName === 'match' ||
+                    child.nodeName === 'url')) {
+                description += `<span class="omnibox-js-${child.nodeName}">`;
+                walk(child);
+                description += "</span>";
+                continue;
+            }
+
+            // Descend into all other nodes, even if they are unrecognized, for
+            // forward compat.
+            walk(child);
+        }
+    };
+    walk(root);
+
+    return description;
 }
